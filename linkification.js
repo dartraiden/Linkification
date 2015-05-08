8b15f2e1b322f21dbc41ab387d8810ad7459bf7a
dartraiden <wowemuh@gmail.com>
2015-05-08 22:01:27 +0300

    Add exception

--- linkification.js
+++ linkification.js
@@ -2,6 +2,17 @@
 // @name           Linkification
 // @namespace      http://userscripts.org/users/splurov/
 // @include        *
+// @exclude        http://www.google.tld/search*
+// @exclude        https://www.google.tld/search*
+// @exclude        http://music.google.com/*
+// @exclude        https://music.google.com/*
+// @exclude        http://mail.google.com/*
+// @exclude        https://mail.google.com/*
+// @exclude        http://docs.google.com/*
+// @exclude        https://docs.google.com/*
+// @exclude        http://mxr.mozilla.org/*
+// @exclude        http://yandex.ru/search*
+// @exclude        https://yandex.ru/search*
 // ==/UserScript==
 
 (function(){
