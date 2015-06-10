01ad7b3b923405b276a702196e8de370a4540e7a
dartraiden <wowemuh@gmail.com>
2015-06-10 11:27:47 +0300

    Add support url

--- linkification.user.js
+++ linkification.user.js
@@ -1,23 +1,24 @@
 ﻿// ==UserScript==
-// @name           Linkification
-// @namespace      http://userscripts.org/users/splurov/
-// @version        20150511.1
-// @description    Turn plain text URLs into clickable links
-// @icon           https://raw.githubusercontent.com/dartraiden/Linkification/master/icon.png
-// @include        *
-// @exclude        http://www.google.tld/search*
-// @exclude        https://www.google.tld/search*
-// @exclude        http://music.google.com/*
-// @exclude        https://music.google.com/*
-// @exclude        http://mail.google.com/*
-// @exclude        https://mail.google.com/*
-// @exclude        http://docs.google.com/*
-// @exclude        https://docs.google.com/*
-// @exclude        http://mxr.mozilla.org/*
-// @exclude        http://yandex.ru/search*
-// @exclude        https://yandex.ru/search*
-// @updateURL	   https://github.com/dartraiden/Linkification/raw/master/linkification.meta.js
-// @downloadURL    https://github.com/dartraiden/Linkification/raw/master/linkification.user.js
+// @name            Linkification
+// @description     Turn plain text URLs into clickable links
+// @namespace       http://userscripts.org/users/splurov/
+// @version         20150610.1
+// @icon            https://raw.githubusercontent.com/dartraiden/Linkification/master/icon.png
+// @include         *
+// @exclude         http://www.google.tld/search*
+// @exclude         https://www.google.tld/search*
+// @exclude         http://music.google.com/*
+// @exclude         https://music.google.com/*
+// @exclude         http://mail.google.com/*
+// @exclude         https://mail.google.com/*
+// @exclude         http://docs.google.com/*
+// @exclude         https://docs.google.com/*
+// @exclude         http://mxr.mozilla.org/*
+// @exclude         http://yandex.ru/search*
+// @exclude         https://yandex.ru/search*
+// @updateURL	    https://github.com/dartraiden/Linkification/raw/master/linkification.meta.js
+// @downloadURL     https://github.com/dartraiden/Linkification/raw/master/linkification.user.js
+// @supportURL	    https://github.com/dartraiden/Linkification/issues
 // ==/UserScript==
 
 (function(){
