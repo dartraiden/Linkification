d9aba265c6a02656de3c32c46e3e690d47703439
dartraiden <wowemuh@gmail.com>
2015-06-10 12:12:12 +0300

    Add yandex.com support, removed icon

--- linkification.user.js
+++ linkification.user.js
@@ -3,7 +3,6 @@
 // @description     Turn plain text URLs into clickable links
 // @namespace       http://userscripts.org/users/splurov/
 // @version         20150610.1
-// @icon            https://raw.githubusercontent.com/dartraiden/Linkification/master/icon.png
 // @include         *
 // @exclude         http://www.google.tld/search*
 // @exclude         https://www.google.tld/search*
@@ -14,6 +13,8 @@
 // @exclude         http://docs.google.com/*
 // @exclude         https://docs.google.com/*
 // @exclude         http://mxr.mozilla.org/*
+// @exclude         http://yandex.com/search*
+// @exclude         https://yandex.com/search*
 // @exclude         http://yandex.ru/search*
 // @exclude         https://yandex.ru/search*
 // @updateURL	    https://github.com/dartraiden/Linkification/raw/master/linkification.meta.js
